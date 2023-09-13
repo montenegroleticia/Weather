69fd86924745cf04bdebfa84d67c4f1a779ed2e3
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-09-13 17:12:24 -0300

    Reset and GlobalStyle

--- src/components/Header/style.jsx
+++ src/components/Header/style.jsx
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
 export const ContainerHeader = styled.div`
+  padding: 40px;
   h1 {
-    color: skyblue;
+    color: #fff;
     text-align: center;
+    font-family: "Roboto";
+    font-size: 40px;
   }
 `;
